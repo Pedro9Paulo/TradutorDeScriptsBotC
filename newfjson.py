60e4c76ffa8858b5338fa8edb2846ee2ec4ec7e4
Pedro9Paulo <ppqbambace@yahoo.com.br>
2023-12-12 11:28:36 -0300

    script que atualiza os cenários para o novo formato (necessario novo formato pra o tradutor funcionar)

---
+++ newfjson.py
@@ -0,0 +1,22 @@
+import json
+import os
+for name in os.listdir("oldingles"):
+	f = open("oldingles/"+name)
+	try:
+		script = json.load(f)
+		f.close()
+		cenario = "["
+		for p in script:
+			if p["id"] == "_meta":
+				cenario += "{"
+				for item in p:
+					cenario += '"' + item + '": "' + p[item] + '", ' 
+				cenario = cenario[:-2] + "},"
+			else:
+				cenario += '"'+ p["id"] +'",'
+	except Exception as erro:
+		print("O script " + name + " tem o defeito: " + str(erro))
+
+	f = open("ingles/"+name, "w", encoding="utf-8")
+	f.write(cenario[:-1]+"]")
+	f.close()
