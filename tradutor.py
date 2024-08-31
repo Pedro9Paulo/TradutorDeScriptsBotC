f9d6723ea42f2e81d3354b3ddf5bff495c3b0927
pedro9paulo <ppqbambace@yahoo.com.br>
2024-08-31 15:48:08 -0300

    executavel de tradução de Scripts

--- tradutor.py
+++ tradutor.py
@@ -4,11 +4,21 @@ from tkinter import filedialog
 import json
 import os
 
-def tradutor(*args):
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def findFile(*args):
 	filename = filedialog.askopenfilename()
-	cenario_string = None
 	cenario_ingles.set(filename)
-	arquivo_ref = open("ptbr/all.json", encoding="utf-8")
+
+def tradutor(*args):
+	cenario_string = None
+	arquivo_ref = open(resource_path("ptbr/all.json"), encoding="utf-8")
 	todosRaw = arquivo_ref.read()
 	todosList = todosRaw[2:-2].split(",\n  {")
 	for i in range(1,len(todosList)):
@@ -19,6 +29,7 @@ def tradutor(*args):
 		todos[p[15:idi]] = p
 		arquivo_ref.close()
 	try:
+		filename = cenario_ingles.get()
 		arquivo = open(filename)
 		script = json.load(arquivo)
 		cenario = "[\n"
@@ -41,16 +52,11 @@ def tradutor(*args):
 	except Exception as erro:
 		cenario_traduzido.set(erro)
 	arquivo.close()
-	try:
-		filenamePortugues = filedialog.asksaveasfilename(defaultextension=".json")
-		f = open(filenamePortugues, "w", encoding="utf-8")
-		f.write(cenario_string[:-2]+"\n]")
-		f.close()
-	except Exception as erro:
-		pass
-
-
-
+	filenamePortugues = filedialog.asksaveasfilename(defaultextension=".json", initialfile=filename[filename.rindex('/')+1:])
+	f = open(filenamePortugues, "w", encoding="utf-8")
+	f.write(cenario_string[:-2]+"\n]")
+	f.close()
+		
 
 root = Tk()
 root.title("Tradutor de Cenários")
@@ -63,13 +69,15 @@ root.rowconfigure(0, weight=1)
 ttk.Label(mainframe, text="Selecione o Cenário que deseja traduzir:").grid(column=1, row=1, sticky=W)
 
 cenario_ingles = StringVar()
-ttk.Label(mainframe, textvariable=cenario_ingles).grid(column=2, row=1, sticky=(W, E))
+cenario_ingles_entry = ttk.Entry(mainframe, width=64, textvariable=cenario_ingles)
+cenario_ingles_entry.grid(column=2, row=1, sticky=(W, E))
 
-cenario_string = ttk.Button(mainframe, text="Traduzir", command=tradutor).grid(column=3, row=1, sticky=W)
+ttk.Button(mainframe, text="Procure", command=findFile).grid(column=3, row=1, sticky=W)
 
 cenario_traduzido = StringVar()
 ttk.Label(mainframe, textvariable=cenario_traduzido).grid(column=2, row=2, sticky=(W, E))
 
+ttk.Button(mainframe, text="Traduzir", command=tradutor).grid(column=3, row=2, sticky=W)
 
 root.bind("<Return>", tradutor)
 
