0a5099c0c029578bf9c090ff37e23f32684b677c
pedro9paulo <ppqbambace@yahoo.com.br>
2024-09-24 12:42:28 -0300

    detalhes visuais finalizados

--- tradutorWeb.py
+++ tradutorWeb.py
@@ -4,8 +4,8 @@ import urllib
 app = Flask(__name__)
 
 def traduz(script):
-    arquivo_ref = open("ptbr/all.json", encoding="utf-8")
-    todosRaw = arquivo_ref.read()
+    arquivo_all = urllib.request.urlopen("https://raw.githubusercontent.com/Pedro9Paulo/TradutorDeScriptsBotC/refs/heads/main/ptbr/all.json")
+    todosRaw = arquivo_all.read().decode("utf-8")
     todosList = todosRaw[2:-2].split(",\n  {")
     for i in range(1,len(todosList)):
         todosList[i] = "  {"+todosList[i]
@@ -13,7 +13,6 @@ def traduz(script):
     for p in todosList:
         idi = p.find("_br")
         todos[p[15:idi]] = p
-        arquivo_ref.close()
 
 
     cenario = "[\n"
@@ -38,17 +37,31 @@ def traduz(script):
 def form():
     return """
         <html>
-            <body>
-                <h1>Tradutor de Scripts</h1>
-
+            <head>
+                <title> Tradutor BotC </title>
+                <link rel="icon" type="imp/png" href="https://raw.githubusercontent.com/Pedro9Paulo/TradutorDeScriptsBotC/main/images/imp.png">
+            </head>
+            <body style="background-color:#210062;color:white;text-align:center">
+                <h1> <img style="vertical-align:middle" src="https://raw.githubusercontent.com/Pedro9Paulo/TradutorDeScriptsBotC/main/images/librarian.png" width="90" height="100"> Tradutor de Cenários de Blood on the Clocktower <img style="vertical-align:middle" src="https://raw.githubusercontent.com/Pedro9Paulo/TradutorDeScriptsBotC/main/images/librarian.png" width="90" height="100"></h1>
+                <br>
+                <p> Envie o Json que deseja traduzir do seu computador: </p>
+                <br>
                 <form action="/tradutor_upload" method="post" enctype="multipart/form-data">
                     <input type="file" name="data_file" />
                     <input type="submit" />
                 </form>
+                <br>
+                <p> OU insira o endereço do link: </p>
+                <br>
                 <form action="/tradutor_link" method="post" enctype="multipart/form-data">
                     <input type="form" name="url" />
                     <input type="submit" />
                 </form>
+                <br>
+                <br>
+                <br>
+                <p> Contribua para esse projeto também: </p> 
+                <p> <a href="https://github.com/Pedro9Paulo/TradutorDeScriptsBotC"> <img src="https://raw.githubusercontent.com/Pedro9Paulo/TradutorDeScriptsBotC/main/images/legion.png" width="90" height="100"> </a></p>
             </body>
         </html>
     """
@@ -70,7 +83,6 @@ def tradutor_upload():
 def tradutor_link():
     request_url = request.form.get("url")
     request_file = urllib.request.urlopen(request_url)
-    print(request_file)
     if not request_file:
         return "No file"
     
